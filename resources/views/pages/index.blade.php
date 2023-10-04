f203dc880dd29e1fe4dd8512379b769c86fb243f
octocatblain <blainmasaku2@gmail.com>
2023-10-04 16:37:05 +0300

    format index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,20 +1,12 @@
-{{-- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
-    <div class="container mt-5">
+    <div class="container mx-auto">
         <div class="text-center mb-4">
             <h1 class="display-5">Users</h1>
         </div>
         <div class="d-flex justify-content-center align-items-center">
-            <livewire:users-table>
+            @livewire('users-table')
         </div>
-    </div>
-@endsection --}}
-
-@extends('layouts.app')
-@section('content')
-    <div class="container mx-auto">
-        <h1 class="text-3xl text-center my-10">Users</h1>
-        <livewire:users-table>
     </div>
 @endsection
