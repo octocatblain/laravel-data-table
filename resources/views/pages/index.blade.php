ee47b57fe10d158fffb1febee769dcbf0e5a3fa7
octocatblain <blainmasaku2@gmail.com>
2023-10-04 15:34:47 +0300

    fix: remove livewire bug

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
     <div class="container mt-5">
@@ -6,7 +6,15 @@
             <h1 class="display-5">Users</h1>
         </div>
         <div class="d-flex justify-content-center align-items-center">
-            @livewire('users-table')
+            <livewire:users-table>
         </div>
+    </div>
+@endsection --}}
+
+@extends('layouts.app')
+@section('content')
+    <div class="container mx-auto">
+        <h1 class="text-3xl text-center my-10">Users</h1>
+        <livewire:users-table>
     </div>
 @endsection
